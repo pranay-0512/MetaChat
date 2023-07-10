5e71f5facb3b964e90c9edc073c167f933194324
Pranay Varma <linkpranayv90@gmail.com>
2023-07-10 17:01:56 +0530

    Updated index.js for login

--- server/index.js
+++ server/index.js
@@ -35,7 +35,49 @@ app.post('/api/register', async(req,res)=>{
             }
         }
     } catch (error) {
-        
+        console.log('error ', error )
+    }
+})
+//for login
+app.post('/api/login', async(req,res)=>{
+    try {
+        const {email, password} = req.body;
+        if(!email || !password){
+            res.status(400).send('Please fill all required fields');
+        }
+        else{
+            const user = await Users.findOne({email});
+            if(!user){
+                res.status(400).send('Email not registered');
+            }
+            else{
+                const validateUser = await bcrypt.compare(password, user.password);
+                if(!validateUser){
+                    res.status(400).send('Password does not match');
+                }
+                else{
+                    const payload = {
+                        userId:user._id,
+                        email:user.email
+                    }
+                    const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'THIS_IS_A_SECRET_KEY';
+                    jwt.sign(payload, JWT_SECRET_KEY, async(err,token)=>{
+                        if(err){
+                            console.log('error ', err);
+                            res.status(500).send('Error generating token')
+                        }
+                        else{
+                            await Users.updateOne({_id:user._id}, {$set:{token}})
+                            user.token = token;
+                            await user.save();
+                            res.status(200).json({user})
+                        }
+                    })
+                }
+            }
+        }
+    } catch (error) {
+        console.log('error ', error )
     }
 })
 
