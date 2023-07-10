f60f98a41e460e8c6e35567a4e5e25de55b0decf
Pranay Varma <linkpranayv90@gmail.com>
2023-07-10 16:35:26 +0530

    Updated index.js for signup

--- server/index.js
+++ server/index.js
@@ -12,6 +12,35 @@ require('./db/connection')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken');
 
+//for signup
+app.post('/api/register', async(req,res)=>{
+    try {
+        const {fullName, email, password} = req.body;
+        if(!fullName || !email || !password){
+            res.status(400).send('Please fill all required details');
+        }
+        else{
+            const alreadyExists = await Users.findOne({email});
+            if(alreadyExists){
+                res.status(400).send('Email already registered, please login')
+            }
+            else{
+                const newUser = new Users({email, fullName});
+                bcrypt.hash(password, 10, (err, hashedPassword)=>{
+                    newUser.set('password', bcrypt.hashedPassword);
+                    newUser.save();
+                    next();
+                })
+                return res.status(200).send('User regisetered successfully')
+            }
+        }
+    } catch (error) {
+        
+    }
+})
+
+
+
 app.listen(PORT, ()=>{
     console.log('Running on PORT:',PORT)
 })
