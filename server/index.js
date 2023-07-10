ec18f6208ba50e9996c3f23bd924c893690d5bde
Pranay Varma <linkpranayv90@gmail.com>
2023-07-10 16:27:14 +0530

    Working on backend

--- server/index.js
+++ server/index.js
@@ -1,2 +1,14 @@
 const express = require('express')
 const cors = require('cors')
+const PORT = process.env.PORT || 8000;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+app.use(cors());
+
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+
+app.listen(PORT, ()=>{
+    console.log('Running on PORT:',PORT)
+})
